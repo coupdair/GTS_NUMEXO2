2a6e179ebdc7f2ff3f72e0bd7038607912359738
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2017-06-28 14:20:14 +0200

    process: add increment (see dbSubExample.c/myAsub*); setup mySubDebug to 1 (see st.cmd).

--- gtsApp/src/dbSubExample.c
+++ gtsApp/src/dbSubExample.c
@@ -8,6 +8,8 @@
 
 int mySubDebug;
 
+static int increment;
+
 static long mySubInit(subRecord *precord)
 {
     if (mySubDebug)
@@ -26,24 +28,37 @@ static long mySubProcess(subRecord *precord)
 
 static long myAsubInit(aSubRecord *precord)
 {
-    if (mySubDebug)
-        printf("Record %s called myAsubInit(%p)\n",
-               precord->name, (void*) precord);
+  increment=0;
+  if (mySubDebug)
+    printf("Record %s called myAsubInit(%p) #%d\n"
+      , precord->name, (void*) precord
+      , increment);
     return 0;
 }
 
 static long myAsubProcess(aSubRecord *precord)
 {
-    if (mySubDebug)
-        printf("Record %s called myAsubProcess(%p)\n",
-               precord->name, (void*) precord);
+  ++increment;
+  if (mySubDebug)
+    printf("Record %s called myAsubProcess(%p) #%d\n"
+      , precord->name, (void*) precord
+      , increment);
     return 0;
 }
 
 /* Register these symbols for use by IOC code: */
 
+//debug
+//e.g.: st.cmd: var mySubDebug 1
+//or interactive: epics> var mySubDebug 0
 epicsExportAddress(int, mySubDebug);
+
+//user called
+//e.g.: caput coudert:subExample 123.456
 epicsRegisterFunction(mySubInit);
 epicsRegisterFunction(mySubProcess);
+
+//periodicaly called
 epicsRegisterFunction(myAsubInit);
 epicsRegisterFunction(myAsubProcess);
+
