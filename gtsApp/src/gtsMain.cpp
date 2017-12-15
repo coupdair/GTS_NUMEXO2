307d64020755cf0e7b67368e793539b04e9e2bca
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2017-12-15 17:22:13 +0100

    EPICS: triggerCoreStart/Stop/Reset added (v0.2.1)

--- gtsApp/src/gtsMain.cpp
+++ gtsApp/src/gtsMain.cpp
@@ -9,7 +9,7 @@
 /**
  * \c GTS_server code version, should be changed by the developper in this \c gtsMain.c C++ file
 **/
-#define VERSION "v0.2.1g"
+#define VERSION "v0.2.1"
 
 /*Additional documentation for the generation of the reference page (using doxygen)*/
 /**
