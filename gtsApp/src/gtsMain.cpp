85df86c1468fe859fa86ce87b91cf03481368882
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2017-12-19 11:22:50 +0100

    EPICS: add major.minor version in :serverVersion.F ; record up to getLatency (v0.2.3)

--- gtsApp/src/gtsMain.cpp
+++ gtsApp/src/gtsMain.cpp
@@ -9,7 +9,7 @@
 /**
  * \c GTS_server code version, should be changed by the developper in this \c gtsMain.c C++ file
 **/
-#define VERSION "v0.2.3g"
+#define VERSION "v0.2.3"
 
 /*Additional documentation for the generation of the reference page (using doxygen)*/
 /**
