f82fb138801cd52f9ace5a1e34f199ef60119833
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2018-01-16 16:07:08 +0100

    GTS id: should have /GTSid file to start, see _info.txt (v0.3.0d)

--- gts.sh
+++ gts.sh
@@ -1,15 +1,16 @@
 #run GTS server either UDP or EPICS on NumExo2 module
+##gts.v0.3.
 
 killall gts
 cd /usr/local/bin/
 
 ##UDP
-#./gts &
+#./gts -i $id &
 
 ##EPICS
-#todo: get IP and set it in gts.cmd
 export EPICS_CA_ADDR_LIST="10.10.17.254"
 ./caRepeater &
-./gts --epics -c gts.cmd
-
+id=`cat /GTSid`
+sed "s/__GTS_ID__/$id/" /usr/local/bin/envGTS_ID > /usr/local/bin/envGTSid
+./gts --epics -c gts.cmd -i $id
 
--- gtsApp/src/gtsMain.cpp
+++ gtsApp/src/gtsMain.cpp
@@ -9,7 +9,7 @@
 /**
  * \c GTS_server code version, should be changed by the developper in this \c gtsMain.c C++ file
 **/
-#define VERSION "v0.2.9d"
+#define VERSION "v0.3.0d"
 
 /*Additional documentation for the generation of the reference page (using doxygen)*/
 /**
